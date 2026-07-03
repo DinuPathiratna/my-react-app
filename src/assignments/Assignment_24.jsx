b7c592ff440eaecf324f4f4425876fe19bf47bb4
Dinuki Pathiratna <dinupathiratna@gmail.com>
2026-07-03 12:18:11 +0530

    add duration component

--- src/assignments/Assignment_24.jsx
+++ src/assignments/Assignment_24.jsx
@@ -1,43 +1,48 @@
 import { useEffect, useState } from "react";
 
-function NumberIncrement({ value }) {
+function NumberIncrement({ value, duration }) {
     const [text, setText] = useState(0);
 
     useEffect(() => {
+        const interval = 20;
+        const increase = value / (duration / interval);
+
         const timer = setInterval(() => {
             setText((current) => {
-                if (current + 100 >= value) {
+                if (current + increase >= value) {
                     clearInterval(timer);
                     return value;
                 }
 
-                return current + 1;
+                return current + increase;
             });
-        }, 1);
+        }, interval);
 
         return () => clearInterval(timer);
-    }, [value]);
-    return <>{text.toLocaleString()}</>;
+    }, [value, duration]);
 
+    return <>{text.toLocaleString()}</>;
 }
 
 export default function Assignment_24() {
     return (
-        <div style={{ display: "flex",
-                     flexDirection: "column",
-                     gap: "20px" ,
-                     fontSize:"40px"}}>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+                fontSize: "40px"}}>
             <div>
-                <NumberIncrement value={500000} />
+                <NumberIncrement value={500000} duration={2000} />
+                </div>
+            <div>
+                <NumberIncrement value={500} duration={2000} />
             </div>
             <div>
-                <NumberIncrement value={500} />
+                <NumberIncrement value={125470} duration={2000} />
             </div>
             <div>
-                <NumberIncrement value={125470} />
-            </div>
-            <div>
-                <NumberIncrement value={75747} />
+                <NumberIncrement value={75747} duration={2000} />
             </div>
         </div>
     );
