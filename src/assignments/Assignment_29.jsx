ad76c845ad37ada9a9a169c1159644a78b9d0aa0
Dinuki Pathiratna <dinupathiratna@gmail.com>
2026-07-16 16:03:04 +0530

    LBFIN: mute slash animation

--- src/assignments/Assignment_29.jsx
+++ src/assignments/Assignment_29.jsx
@@ -23,13 +23,19 @@ function Assignment_29() {
                     className="wave wave1"
                     d="M42 24 Q50 32 42 40"
                 />
+
                 <path
                     className="wave wave2"
                     d="M48 18 Q60 32 48 46"
                 />
 
-                <line className="cross cross1" x1="42" y1="22" x2="56" y2="42" />
-                <line className="cross cross2" x1="56" y1="22" x2="42" y2="42" />
+                <line
+                    className="slash"
+                    x1="16"
+                    y1="48"
+                    x2="50"
+                    y2="16"
+                />
             </svg>
         </button>
     );
